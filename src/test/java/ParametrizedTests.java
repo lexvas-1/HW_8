841227a10be660a2978ec6c10502b285058c4a44
lexvas-1 <lexsatellite@yandex.ru>
2024-08-20 17:06:15 +0300

    add 3 sources

--- src/test/java/ParametrizedTests.java
+++ src/test/java/ParametrizedTests.java
@@ -4,13 +4,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.ValueSource;
-
 import static com.codeborne.selenide.Condition.appear;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
+
 
 @DisplayName("Набор параметризированных тестов для ИМ 123")
 public class ParametrizedTests {
@@ -27,6 +28,8 @@ public class ParametrizedTests {
     })
     @ParameterizedTest(name = "Добавление из категории {0}")
     @Tag("Smoke")
+    @Tag("High")
+    @Tag("Команда 1")
     void addToCartFromCategoriesTest(String category) {
         open(category);
         $(".buy-block").$(byText("в корзину")).click();
@@ -40,10 +43,31 @@ public class ParametrizedTests {
 
     @ParameterizedTest(name = "Добавление из КТ артикула {0}")
     @Tag("Smoke")
+    @Tag("Medium")
+    @Tag("Команда 1")
     void addToCardFromSearchTest(String articul) {
         $("#searchinput").setValue(articul).pressEnter();
         $(".pc-button-pay").click();
         $("#basket-preview").shouldBe(appear);
         $("#basket-preview").shouldHave(text("Товар добавлен в корзину"));
     }
+
+
+    @DisplayName("Смена региона в хедере")
+    @CsvSource(value = {"Санкт-Петербург, 8 (800) 333 9 123", "Москва, 8 (495) 225-9-123"
+    })
+    @ParameterizedTest(name = "В хедере регион {0} имеет телефон {1}")
+    @Tag("Smoke")
+    @Tag("Medium")
+    @Tag("Команда 2")
+    void checkRegionHeaderTest(String city, String number) {
+        open("/");
+        $(".select-city").click();
+        $("#cities").$(".city-list").$(byText(city)).click();
+        $(".select-city").shouldHave(text(city));
+        $(".wr__tel").shouldHave(text(number));
+
+    }
+
+
 }
